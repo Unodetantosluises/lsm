f122de5c82a465dccc24ffb3ac700fe86b367867
Luis Diaz <luisantoniodiaz97@gmail.com>
2025-01-08 20:50:01 -0600

    Update: UserServiceImpl

--- src/main/java/com/languajesignalgroup/lsm_web/services/impl/UserServiceImpl.java
+++ src/main/java/com/languajesignalgroup/lsm_web/services/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.languajesignalgroup.lsm_web.services.impl;
 
 import com.languajesignalgroup.lsm_web.dto.RegistrationDto;
+import com.languajesignalgroup.lsm_web.models.Role;
 import com.languajesignalgroup.lsm_web.models.User;
 import com.languajesignalgroup.lsm_web.repository.RoleRepository;
 import com.languajesignalgroup.lsm_web.repository.UserRepository;
@@ -9,7 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -29,7 +31,11 @@ public class UserServiceImpl implements UserService {
         User user = new User();
         user.setUserName(registrationDto.getUserName());
         user.setUserLastName(registrationDto.getUserLastName());
-        user.getBirthdateUser(registrationDto.getBirthdateUser());
+        user.setBirthdateUser(LocalDate.from(registrationDto.getBirthdateUser()));
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
+        Role role = roleRepository.findByName("STUDENT");
+        user.setRoles(Arrays.asList(role));
+        userRepository.save(user);
 
     }
 
